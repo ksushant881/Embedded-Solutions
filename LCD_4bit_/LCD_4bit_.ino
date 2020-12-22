aa1ab38255d209dc380e56fd2bf8dc1768c9d1c8
ksushant881 <71615295+ksushant881@users.noreply.github.com>
2020-12-22 22:40:53 +0530

    Updated on 22/12/2020

--- LCD_4bit_/LCD_4bit_.ino
+++ LCD_4bit_/LCD_4bit_.ino
@@ -1,6 +1,6 @@
-//8bit mode
+//4bit mode
 #include<LiquidCrystal.h>
-const int rs=9 ,en=8 ,d0=0,d1=1,d2=2,d3=3,d4=4,d5=5,d6=6,d7=7;
+const int rs=9 ,en=8 ,d0=0,d1=1,d2=2,d3=3;
 LiquidCrystal lcd(rs,en,d0,d1,d2,d3,d4,d5,d6,d7);
 void setup() {
  lcd.begin(16,2);
@@ -12,11 +12,15 @@ void setup() {
 
 void loop(){
   lcd.setCursor(5,1);
-  // lcd.print(millis());//displays time of operation
+  lcd.print(millis());
   delay(1000);
   lcd.clear();
   lcd.setCursor(0,1);
   lcd.print("Cleared");
   delay(1000);
+//  for(int p=0;p<16;p++){
+// // lcd.scrollDisplayLeft(); //to make a marquee effect
+// delay(150);
+//  }
 
 }
